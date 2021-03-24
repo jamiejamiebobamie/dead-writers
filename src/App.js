b98ff4751d859299e329ce9be23ec82532e241fb
jamiejamiebobamie <jmccrory@vt.edu>
2021-03-23 21:28:01 -0400

    added a means of changing the favicon with webpack not working.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BackgroundVideoContainer } from "./components";
 import { ControlsContainer } from "./components";
 import { QuoteDisplayContainer } from "./components";
 
-import { isMobile } from "react-device-detect";
+import favicon from "-!file-loader!../assets/favicon.ico";
 
 import "./App.css";
 
 export const App = () => {
+  useEffect(() => {
+    const el = document.getElementById("favicon");
+    el.href = favicon;
+  }, []);
+
   return (
     <div className="App">
       <ControlsContainer />
